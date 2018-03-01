230eedb033d39a392b8fd870d35d42404145ab7c
Nitin Chauhan <nitinchauhan2093@gmail.com>
2018-03-01 17:04:47 +0000

    Newton Square Root

--- newton_sqrt.py
+++ newton_sqrt.py
@@ -1,8 +1,9 @@
-#Python function named sqrt_newton(x) that implements Newton's method to compute the square root
-def sqrt_newton(x):
-    guess = x/2
-    while (guess**2-x)>10**-12:
-        guess = (guess + x/guess) * 0.5
+#Python function named sqrt_newton(num) that implements Newton's method to compute the square root
+def sqrt_newton(num):
+    guess = num/2
+    while (guess**2-num)>10**-12:
+        guess = (guess + num/guess) * 0.5
     return guess
-x = int(input("Enter a non-negative number\t"))
-print(sqrt_newton(x))
+#Creating a variable num that is assigned a non-negative number entered by the user
+num = int(input("Enter a non-negative number\t"))
+print(sqrt_newton(num))
