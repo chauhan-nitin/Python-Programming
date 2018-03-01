7f379239c3d473d2409ee1df1bdfea2c9124a2b2
Nitin Chauhan <nitinchauhan2093@gmail.com>
2018-03-01 17:05:21 +0000

    Concept - Sequence Programs

--- sequence.py
+++ sequence.py
@@ -1,24 +1,33 @@
-#Python function all_equal(s) return True if all elements in sequence s the same as one another and False otherwise.
-def all_equal(s):
-    f=0
-    l = len(s)
-    if (l==0)or (l==1):
-        f=1
-    else:
-        for i in range(0,l-1):
-            if s[i]==s[i+1]:
-                f=1
-            else:
-                f=0
-    if(f==1):
-        print("TRUE\n")
-    else:
-        print("FALSE\n")
+#Python function all_equal(s) return True if all elements in sequence 'S' are same as one another and False otherwise.
+#Here the sequence can be described as [1,1,1] or [2,6] or [8,8,8,8] or [3]
+def all_equal(S):
+    #flagging variable to check whether Sequence elements are same or not
+    flag=0
 
-all_equal([1])
-all_equal([2,4,2])
-all_equal([2,2,2,2])
-all_equal([])
+    #length of the sequence 'S'
+    S_length = len(S)
+    
+    #Condition will be true if sequence 'S' has one or no element
+    if (S_length==0)or (S_length==1):
+        flag=1
+    else:
+        for digit in range(0,S_length-1):
+            if S[digit]==S[digit+1]:
+                flag=1
+            else:
+                flag=0
+    #test condition for flagging variable to return final result
+    if(flag==1):
+        return("TRUE\n")
+    else:
+        return("FALSE\n")
+
+#Calling the function with different trial of sequences inside print function
+print(all_equal([1]))
+print(all_equal([2,4,2]))
+print(all_equal([2,2,2,2]))
+print(all_equal([]))
+
 
 #Python function named all_pairs(s1,s2) Output, on separate lines, all pairs of elements from sequences s1 and s2, respectively,
 #with the second element varying more rapidly than the first.
@@ -32,17 +41,20 @@ def all_pairs(s1,s2):
 
 all_pairs([1,2],"abc")
 
-#Python function named distinct(s) return the string formed from the first occurrence of each element in string s.
-def distinct(s):
+
+#Python function named distinct(s) return the string formed from the first occurrence of each element in string 'S'.
+def distinct(S):
+    #empty string to hold first occurence of each element in string 'S'
     pos = ""
-    for i in s:
+    for i in S:
         if i in pos:
             continue
         else:
             pos+=i
     return pos
-s = input("Enter any String  ")
-print(distinct(s))
+#Creating a variable 'S' to hold the input string from the user
+S = input("Enter any String  ")
+print(distinct(S))
 
 
             
